eba20b194f329a0673ec5bd1ad29f5ddc4795647
Madhusudhan V <madhusudhan.sv@gmail.com>
2025-06-24 17:13:45 +0100

    Adding autocorrect suggestions

--- src/main/java/com/madhu/qou/dto/FacetedSearchResponse.java
+++ src/main/java/com/madhu/qou/dto/FacetedSearchResponse.java
@@ -1,10 +1,14 @@
 package com.madhu.qou.dto;
 
 import com.madhu.qou.dto.domain.Product;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import java.util.List;
 
 // The main response object
 public record FacetedSearchResponse(
         List<Product> products,
-        List<Facet> facets
+        List<Facet> facets,
+        // Add a field for our suggestion. It will only appear in the JSON if it's not null.
+        @JsonInclude(JsonInclude.Include.NON_NULL)
+        String didYouMeanSuggestion
 ) {}
